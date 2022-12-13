f5a1325676ecde0f69e64dacd871527527d76952
AlfonsoV <ravgamerica@hotmail.com>
2022-12-13 13:31:33 -0600

    Allow Origin

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config();
-const scoreRoutes = require('./src/scores')
+const scoreRoutes = require('./src/scores');
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
 // middleware
-app.use(express.json())
-app.use('/api', scoreRoutes)
+app.use(express.json());
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Authorization, Content-Type');
+    next();
+});
+app.use('/api', scoreRoutes);
 
 // Conexion a mongo
 mongoose.connect(process.env.MONGODB_URI)
@@ -16,5 +22,5 @@ mongoose.connect(process.env.MONGODB_URI)
     .catch(() => console.log(error))
 
 //Ruta obtener score
-app.get('/', (req, res) => res.send('API para obtener scores y subir scores a la base de datos en el juego attack space. Desarrollado por AlfonsoV'))
-app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}!`))
+app.get('/', (req, res) => res.send('API para obtener scores y subir scores a la base de datos en el juego attack space. Desarrollado por AlfonsoV'));
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}!`));
